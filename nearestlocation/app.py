0e4add3a3534a0510ec26c1656927222c69af68f
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2020-10-23 09:35:35 +0700

    Update app.py

--- nearestlocation/app.py
+++ nearestlocation/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 ####################### new ########################
 @app.route('/')
-def index(name):
-  return render_template('myTemplate.html', name=name)
+def index():
+  return render_template('myTemplate.html')
 
