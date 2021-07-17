87333971c14ef3f162ffa1460eb30e3c6f3bb7c6
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-07-18 06:42:26 +0700

    Update app.py

--- nearestlocation/app.py
+++ nearestlocation/app.py
@@ -1,26 +1,12 @@
 
-from flask import Flask, request, render_template
-import json
-import requests
+from flask import Flask #need
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "hello"
-
-
-#if __name__ == '__main__':
- # app.run(debug=True)
-
-#from flask import Flask #need
-
-#app = Flask(__name__)
 
 
 
 ####################### new ########################
-#@app.route('/')
-#def index():
-  #return "My first python on Git Heroku"
+@app.route('/')
+def index():
+  return "My first python on Git Heroku"
   #return "Helloooooo"
