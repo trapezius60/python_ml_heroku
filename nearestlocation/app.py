0b080587d96cf8cc327a375001f2975280948f30
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-07-18 06:29:44 +0700

    Update app.py

--- nearestlocation/app.py
+++ nearestlocation/app.py
@@ -2,39 +2,12 @@
 from flask import Flask, request, render_template
 import json
 import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "hello"
-
-# ส่วน callback สำหรับ Webhook
-@app.route('/callback', methods=['POST'])
-def callback():
-  json_line = request.get_json()
-  json_line = json.dumps(json_line)
-  decoded = json.loads(json_line)
-  user = decoded["events"][0]['replyToken']
-  #id=[d['replyToken'] for d in user][0]
-  #print(json_line)
-  print("ผู้ใช้：",user)
-  sendText(user,'งง') # ส่งข้อความ งง
-  return '',200
-
-
-def sendText(user, text):
-  LINE_API = 'https://api.line.me/v2/bot/message/reply'
-  Authorization = 'Bearer ENTER_ACCESS_TOKEN' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
-  headers = {
-  'Content-Type': 'application/json; charset=UTF-8',
-  'Authorization':Authorization
-  }
-  data = json.dumps({
-  "replyToken":user,
-  "messages":[{"type":"text","text":text}]})
-  #print("ข้อมูล：",data)
-  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
-  #print(r.text)
+    return "hello"
 
 
 if __name__ == '__main__':
