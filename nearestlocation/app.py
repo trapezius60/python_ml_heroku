672c4e72cba81a4affdfd9c57677dd0d1d9bd478
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-07-17 22:44:23 +0700

    Update app.py

--- nearestlocation/app.py
+++ nearestlocation/app.py
@@ -7,8 +7,8 @@ import requests
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('xxxxxxxx')
-handler = WebhookHandler('xxxxxxxx')
+line_bot_api = LineBotApi('t8TS42nUWRlHempLf4OLMEf1xoNm96YHojEt71MgX96NGuA9qucXNT/4nJtBscYdXZt/ADJLVbqfcwIbdSrlqsW0s0z6i8GPPWtipaaGnOoj0UhNrGI7eeOXAzRf4A6s1hdq+CraBNPxexpYI3TwowdB04t89/1O/w1cDnyilFU=') #Channel Access Token line
+handler = WebhookHandler('7f819199fc35d2461ceb0191d0fb304d') #Channel Secret of line
 
 @app.route("/callback", methods=['POST'])
 
