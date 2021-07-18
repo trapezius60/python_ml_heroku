e07eb6b316607481c7065f2f860d9c28f277007c
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-07-18 18:57:17 +0700

    Update app.py

--- nearestlocation/app.py
+++ nearestlocation/app.py
@@ -40,19 +40,6 @@ def hello():
 def get_api():
     return jsonify(data)
 
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
-app = Flask(__name__)
 
 line_bot_api = LineBotApi('t8TS42nUWRlHempLf4OLMEf1xoNm96YHojEt71MgX96NGuA9qucXNT/4nJtBscYdXZt/ADJLVbqfcwIbdSrlqsW0s0z6i8GPPWtipaaGnOoj0UhNrGI7eeOXAzRf4A6s1hdq+CraBNPxexpYI3TwowdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('7f819199fc35d2461ceb0191d0fb304d')
