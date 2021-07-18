da74d3e574536b2d87adb918b8f8310861f6083e
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-07-18 07:31:33 +0700

    Update app.py

--- nearestlocation/app.py
+++ nearestlocation/app.py
@@ -1,12 +1,46 @@
 
-from flask import Flask #need
+#from flask import Flask #need
+
+#app = Flask(__name__)
+
+
+
+
+#@app.route('/')
+#def index():
+ # return "Hellowwwwwwwwwwwwwwwwwwwwwwwwwwwwww"
+  #return "Helloooooo"
+  
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+data = [
+        {
+            "id": 1,
+            "library": "Pandas",
+            "language": "Python"
+        },
+        {
+            "id": 2,
+            "library": "requests",
+            "language": "Python"
+        },
+        {
+            "id": 3,
+            "library": "NumPy",
+            "language": "Python"
+        }
+    ]
 
-
-####################### new ########################
 @app.route('/')
-def index():
-  return "Hellowwwwwwwwwwwwwwwwwwwwwwwwwwwwww"
-  #return "Helloooooo"
+def hello():
+    return "Hello Flask-Heroku"
+
+
+@app.route('/api', methods=['GET'])
+def get_api():
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=False)
