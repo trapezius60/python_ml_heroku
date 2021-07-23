b749c1e0c1855394b1c984882edc87e225b90602
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-07-23 11:07:24 +0700

    Update app.py

--- nearestlocation/app.py
+++ nearestlocation/app.py
@@ -36,7 +36,7 @@ def get_api():
     return jsonify(data)
 
 @app.route('/home')
-def home():
+def main():
     return render_template('index.html')
            
 line_bot_api = LineBotApi('t8TS42nUWRlHempLf4OLMEf1xoNm96YHojEt71MgX96NGuA9qucXNT/4nJtBscYdXZt/ADJLVbqfcwIbdSrlqsW0s0z6i8GPPWtipaaGnOoj0UhNrGI7eeOXAzRf4A6s1hdq+CraBNPxexpYI3TwowdB04t89/1O/w1cDnyilFU=')
