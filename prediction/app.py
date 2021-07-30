1ce71cba65f6295e9ad6a834387f865f38418173
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-07-30 10:36:11 +0700

    Update app.py

--- prediction/app.py
+++ prediction/app.py
@@ -82,36 +82,36 @@ handler = WebhookHandler('7f819199fc35d2461ceb0191d0fb304d')
 
 @app.route("/callback", methods=['POST','GET'])
 def webhook():
-            if request.method == 'POST':
-                        payload = request.json
-                        Reply_token = payload['events'][0]['replyToken']
-                        print(Reply_token)
-                        message = payload['events'][0]['message']['text']
-                        print(message)
-                        if 'ดี' in message :
-                        Reply_messasge = 'ดีมาก'
-                        ReplyMessage(Reply_token,Reply_messasge,'t8TS42nUWRlHempLf4OLMEf1xoNm96YHojEt71MgX96NGuA9qucXNT/4nJtBscYdXZt/ADJLVbqfcwIbdSrlqsW0s0z6i8GPPWtipaaGnOoj0UhNrGI7eeOXAzRf4A6s1hdq+CraBNPxexpYI3TwowdB04t89/1O/w1cDnyilFU=') #ใส่ Channel access token
-                        return request.json, 200
-            else:
-                        abort(400)
+    if request.method == 'POST':
+       payload = request.json
+       Reply_token = payload['events'][0]['replyToken']
+       print(Reply_token)
+       message = payload['events'][0]['message']['text']
+       print(message)
+       if 'ดี' in message :
+           Reply_messasge = 'ดีมาก'
+           ReplyMessage(Reply_token,Reply_messasge,'t8TS42nUWRlHempLf4OLMEf1xoNm96YHojEt71MgX96NGuA9qucXNT/4nJtBscYdXZt/ADJLVbqfcwIbdSrlqsW0s0z6i8GPPWtipaaGnOoj0UhNrGI7eeOXAzRf4A6s1hdq+CraBNPxexpYI3TwowdB04t89/1O/w1cDnyilFU=') #ใส่ Channel access token
+       return request.json, 200
+    else:
+       abort(400)
 def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
-            LINE_API = 'https://api.line.me/v2/bot/message/reply'
-            Authorization = 'Bearer {}'.format(Line_Acees_Token)
-            print(Authorization)
-            headers = {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization':Authorization
-            }
-            data = {
-            "replyToken":Reply_token,
-            "messages":[{
-            "type":"text",
-            "text":TextMessage
-            }]
-            }
-            data = json.dumps(data)
-            r = requests.post(LINE_API, headers=headers, data=data)
-            return 200
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    Authorization = 'Bearer {}'.format(Line_Acees_Token)
+    print(Authorization)
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':Authorization
+     }
+     data = {
+     "replyToken":Reply_token,
+     "messages":[{
+      "type":"text",
+      "text":TextMessage
+      }]
+      }
+      data = json.dumps(data)
+      r = requests.post(LINE_API, headers=headers, data=data)
+      return 200
 
 
 if __name__ == "__main__":
